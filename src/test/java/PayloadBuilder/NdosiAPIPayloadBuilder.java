f9f7a36acc5c2f689622d225261725f76a32ea3e
User <breakathi@gmail.com>
2025-10-22 21:57:10 +0200

    registerPayload

--- src/test/java/PayloadBuilder/NdosiAPIPayloadBuilder.java
+++ src/test/java/PayloadBuilder/NdosiAPIPayloadBuilder.java
@@ -5,14 +5,55 @@ import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import java.sql.PreparedStatement;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class NdosiAPIPayloadBuilder {
 
-    public static JSONObject loginPayload(String email,String password) {
+    public static JSONObject registerPayload(String firstName, String lastName, String email, String password, String confirmPassword) {
+        JSONObject dataObject = new JSONObject();
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("success", true);
+        jsonObject.put("message", "User registered successfully");
+        jsonObject.put("data", dataObject);
+
+        dataObject.put("id", "6eee2902-33b9-4fc4-927f-f83cc81d7189");
+        dataObject.put("firstName", firstName);
+        dataObject.put("lastName", lastName);
+        dataObject.put("email", email);
+        dataObject.put("password", password);
+        dataObject.put("confirmPassword", confirmPassword);
+
+        String createdAt = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        dataObject.put("createdAt", createdAt);
+
+        return jsonObject;
+    }
+
+    public static JSONObject loginPayload(String email, String password) {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("email", email);
         jsonObject.put("password", password);
 
         return jsonObject;
     }
+
+//    @Test
+//    public void testLogin() {
+//        System.out.println(loginPayload("john.doeABCD@example.com", "SecurePass123!"));
+//    }
+//
+//    @Test
+//    public void testRegister() {
+//        System.out.println(registerPayload(
+//                "John",
+//                "Doe",
+//                "john.doeABCD@example.com",
+//                "SecurePass123!",
+//                "SecurePass123!"
+//        ));
+//
+//    }
+
 }
