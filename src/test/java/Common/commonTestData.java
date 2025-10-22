1fe568bd4b0789ab9c0ae20ebb3ce02ce078bd15
Nkosi <njceles@gmail.com>
2025-10-22 18:25:28 +0200

    created base url for ndosi api

---
+++ src/test/java/Common/commonTestData.java
@@ -0,0 +1,8 @@
+package Common;
+
+public class commonTestData {
+
+    public static String NdosiBaseUrl = "https://www.ndosiautomation.co.za";
+    public static String WeatherBaseURl = "https://weather";
+
+}
