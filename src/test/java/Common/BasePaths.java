4128b31fc1f1c35b07fd96f8439007cabcc74461
Nkosi <njceles@gmail.com>
2025-10-22 18:27:59 +0200

    created commom test data

--- src/test/java/Common/BasePaths.java
+++ src/test/java/Common/BasePaths.java
@@ -1,4 +1,8 @@
 package Common;
 
 public class BasePaths {
+
+    public static String NdosiBaseUrl = "https://www.ndosiautomation.co.za";
+    public static String WeatherBaseURl = "https://weather";
+
 }
--- src/test/java/Common/commonTestData.java
+++ src/test/java/Common/commonTestData.java
@@ -2,7 +2,10 @@ package Common;
 
 public class commonTestData {
 
-    public static String NdosiBaseUrl = "https://www.ndosiautomation.co.za";
-    public static String WeatherBaseURl = "https://weather";
+    public static int success_status_code = 200;
+    public static int not_found_status_code = 404;
+    public static int bad_request_status_code = 400;
+    public static int create_success_status_code = 201;
+
 
 }
